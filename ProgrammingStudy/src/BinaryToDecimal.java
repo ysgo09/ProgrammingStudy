273b289039b4c4fb55151e25043b9998f2af8084
ysgo09 <kibum1223@naver.com>
2015-08-02 22:35:10 +0900

    Update BinaryToDecimal.java

--- ProgrammingStudy/src/BinaryToDecimal.java
+++ ProgrammingStudy/src/BinaryToDecimal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BinaryToDecimal {
+public class DecimaltoBinary {
 	static Scanner key=new Scanner(System.in);  
 	public static void main(String[] args){
 		int num=key.nextInt();
